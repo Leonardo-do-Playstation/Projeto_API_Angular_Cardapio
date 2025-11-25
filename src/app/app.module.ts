5641044c1605b0bb9b26543b8fd9e09b9e67ca28
Leonardo Antonio de Oliveira Boff <leooliveiraboff@gmail.com>
2025-11-25 17:22:08 -0300

    Cria estrutura de pastas

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DishesComponent } from './models/dishes/dishes.component';
+import { DrinksComponent } from './models/drinks/drinks.component';
+import { NavbarComponent } from './models/navbar/navbar.component';
+import { HeaderComponent } from './models/header/header.component';
+import { FooterComponent } from './models/footer/footer.component';
+import { HomeComponent } from './models/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DishesComponent,
+    DrinksComponent,
+    NavbarComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
