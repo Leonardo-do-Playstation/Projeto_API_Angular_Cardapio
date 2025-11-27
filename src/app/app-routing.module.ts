08dc1e5b9de99d729aaac607921dab0789a3cb27
Aluno <aluno@fatecitu.local>
2025-11-27 08:41:47 -0300

    Adiciona NavBar e Configura rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DishesComponent } from './models/dishes/dishes.component';
+import { HomeComponent } from './models/home/home.component';
+import { DrinksComponent } from './models/drinks/drinks.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },       
+  { path: 'home', component: HomeComponent },
+  { path: 'pratos', component: DishesComponent },
+  { path: 'bebidas', component: DrinksComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
